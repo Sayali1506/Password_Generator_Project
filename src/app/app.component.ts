22c03d1d53f1a96f57dd676de3e63936b88d6b0e
Sayali Chitale <schitale710@gmail.com>
2022-03-14 16:29:00 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,53 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Practise-Password-Generator';
+  password: string = '';
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+  length = 0;
+
+  onChangeLength(value: number) {
+    if (!isNaN(value)) {
+      this.length = value;
+    }
+    // console.log('not a number please enter a number');
+  }
+
+  onUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '1234567890';
+    const symbols = '!@#$%&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatePassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatePassword += validChars[index];
+    }
+    this.password = generatePassword;
+  }
 }
