0be984971aeb3ddd3b8645ed585ec5df5aa3776b
Sayali Chitale <schitale710@gmail.com>
2022-03-14 17:13:37 +0530

    Version Fix for browser lists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  [x: string]: any;
   password: string = '';
   includeLetters: boolean = false;
   includeNumbers: boolean = false;
